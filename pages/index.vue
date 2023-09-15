bb9257cfdedb3d342272ca4d84c70a351ee75711
Fiyinfoluwa John Ajala <johnajala204@gmail.com>
2023-09-15 20:56:42 +0100

    test

--- pages/index.vue
+++ pages/index.vue
@@ -5,7 +5,12 @@
             <p>Join millions of students, researchers and professionals to instantly answer questions and understand
                 research with AI</p>
 
-            <Button>
+            <Button class="mx-auto">
+                <svg class="inline-block mr-1" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                    viewBox="0 0 24 24">
+                    <path fill="currentColor"
+                        d="M20.283 10.356h-8.327v3.451h4.792c-.446 2.193-2.313 3.453-4.792 3.453a5.27 5.27 0 0 1-5.279-5.28a5.27 5.27 0 0 1 5.279-5.279c1.259 0 2.397.447 3.29 1.178l2.6-2.599c-1.584-1.381-3.615-2.233-5.89-2.233a8.908 8.908 0 0 0-8.934 8.934a8.907 8.907 0 0 0 8.934 8.934c4.467 0 8.529-3.249 8.529-8.934c0-.528-.081-1.097-.202-1.625z" />
+                </svg>
                 Login to get started
             </Button>
         </section>
